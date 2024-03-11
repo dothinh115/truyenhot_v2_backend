d9183a42cfded41a5dc2319894a11267bc50c4dc
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-03-12 01:13:36 +0700

    update ref check

--- src/core/mongoose/plugins/reference-check.plugin.ts
+++ src/core/mongoose/plugins/reference-check.plugin.ts
@@ -1,7 +1,8 @@
 import { Schema } from 'mongoose';
 
 export default function referenceCheckPlugin<T>(schema: Schema<T>) {
-  schema.pre('validate', async function (next) {
+  schema.pre(['save'], async function (next) {
+    //tìm những field nào đang có ref
     const refFields: {
       field: string;
       refTo: string;
@@ -15,12 +16,50 @@ export default function referenceCheckPlugin<T>(schema: Schema<T>) {
         refFields.push(refField);
       }
     }
+    //chạy qua để check ref xem có tồn tại hay ko
     if (refFields.length > 0) {
       for (const fieldObj of refFields) {
         const model = this.model(fieldObj.refTo);
         const value = this.get(fieldObj.field);
-        if (Array.isArray(value) && value.length === 0) return next();
-        if (!value) return next();
+        //nếu giá trị của field là mảng, và mảng rỗng thì next
+        if (Array.isArray(value) && value.length === 0) continue;
+        //nếu ko phải mảng và value = null hoặc undefined thì cũng next
+        if (!value) continue;
+        const exists = await model.exists({ _id: value });
+        if (!exists)
+          throw new Error(
+            `Giá trị ${value} không tồn tại bên trong ${fieldObj.refTo}`,
+          );
+      }
+    }
+    next();
+  });
+
+  schema.pre('findOneAndUpdate', async function (next) {
+    //tìm những field nào đang có ref
+    const refFields: {
+      field: string;
+      refTo: string;
+    }[] = [];
+    for (const field in this.model.schema.paths) {
+      if (this.model.schema.paths[field].options.ref) {
+        const refField = {
+          field,
+          refTo: this.model.schema.paths[field].options.ref,
+        };
+        refFields.push(refField);
+      }
+    }
+    //chạy qua để check ref xem có tồn tại hay ko
+    if (refFields.length > 0) {
+      for (const fieldObj of refFields) {
+        const model = this.model.db.model(fieldObj.refTo);
+        const payload = this.getUpdate();
+        const value = payload[fieldObj.field];
+        //nếu giá trị của field là mảng, và mảng rỗng thì next
+        if (Array.isArray(value) && value.length === 0) continue;
+        //nếu ko phải mảng và value = null hoặc undefined thì cũng next
+        if (!value) continue;
         const exists = await model.exists({ _id: value });
         if (!exists)
           throw new Error(
