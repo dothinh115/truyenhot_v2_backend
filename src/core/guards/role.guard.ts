2114407363fefd00da3ae36ac33435775e71face
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-03-21 02:55:32 +0700

    update

--- src/core/guards/role.guard.ts
+++ src/core/guards/role.guard.ts
@@ -28,7 +28,6 @@ export class RolesGuard implements CanActivate {
     } else {
       url = originalUrl.match(/^\/[^\/?]+/)[0].split('/')[1];
     }
-
     const currentRoutePermission = await this.permissionModel.findOne({
       path: url,
       method: method.toLowerCase(),
