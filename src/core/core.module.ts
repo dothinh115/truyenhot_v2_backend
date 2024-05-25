ee384e830aee52c091fab053dc32b537e8224494
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-05-26 02:37:19 +0700

    update

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -32,12 +32,10 @@ import { redisStore } from 'cache-manager-redis-yet';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    CacheModule.register({
-      store: redisStore,
-      isGlobal: true,
-      // Store-specific configuration:
-      host: '127.0.0.1',
-      port: 6379,
+    CacheModule.registerAsync({
+      useFactory: async () => ({
+        url: 'redis://127.0.0.1:6379',
+      }),
     }),
     MongooseModule.forRoot(process.env.DB_URI, {
       dbName: process.env.DB_NAME,
