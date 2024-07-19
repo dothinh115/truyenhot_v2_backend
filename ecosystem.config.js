73a9f8aa0604e53d3049bd59978201748438bb78
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-07-19 08:18:49 +0700

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'truyenhot-backend',
+      script: 'dist/main.js',
+      instances: '4', // Số lượng instances tối đa sẽ bằng số CPU trên máy của bạn
+      exec_mode: 'cluster', // Chạy trong chế độ cluster
+      watch: false, // Tắt chế độ watch để tránh vấn đề khi cluster
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 4567, // Đặt cổng cho ứng dụng trong môi trường production
+      },
+    },
+  ],
+};
