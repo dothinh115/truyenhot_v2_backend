4de6e065c7db1a61796a2cb7112ca16309eae910
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-07-27 22:04:56 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'truyenhot-backend',
-      script: 'dist/src/main.js',
+      script: 'dist/main.js',
       instances: '4',
       exec_mode: 'cluster',
       watch: false,
