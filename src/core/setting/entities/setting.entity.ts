9f0b9b03f56a0c8e469cb7a896411fe5bfc5d506
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-07-30 04:49:50 +0700

    update

--- src/core/setting/entities/setting.entity.ts
+++ src/core/setting/entities/setting.entity.ts
@@ -1,9 +1,11 @@
+import { Disabled } from 'src/core/decorators/disabled.decorator';
 import { Role } from 'src/core/role/entities/role.entity';
 import { Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Setting {
   @PrimaryGeneratedColumn('uuid')
+  @Disabled()
   id: string;
 
   @OneToOne(() => Role, { eager: true })
