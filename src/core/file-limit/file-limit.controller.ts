64cf78061a17f31c1826bc3555be6527bbe88ca5
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-08-08 23:08:50 +0700

    update

--- src/core/file-limit/file-limit.controller.ts
+++ src/core/file-limit/file-limit.controller.ts
@@ -2,17 +2,20 @@ import { Controller, Get, Body, Patch, Param, Query } from '@nestjs/common';
 import { FileLimitService } from './file-limit.service';
 import { UpdateFileLimitDto } from './dto/update-file-limit.dto';
 import { TQuery } from '../utils/model.util';
+import { Protected } from '../decorators/protected-route.decorator';
 
 @Controller('fileLimit')
 export class FileLimitController {
   constructor(private readonly fileLimitService: FileLimitService) {}
 
   @Get()
+  @Protected()
   findAll(@Query() query: TQuery) {
     return this.fileLimitService.find(query);
   }
 
   @Patch(':id')
+  @Protected()
   update(
     @Param('id') id: string,
     @Body() body: UpdateFileLimitDto,
