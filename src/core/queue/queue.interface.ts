8f86b0b7379b6fff7a2f770348a26572afa4570e
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-08-17 12:43:14 +0700

    update

--- src/core/queue/queue.interface.ts
+++ src/core/queue/queue.interface.ts
@@ -3,7 +3,7 @@ import BeeQueue from 'bee-queue';
 export interface IQueue {
   queue: BeeQueue;
 
-  add(data: string): Promise<BeeQueue.Job<any>>;
+  add(data: string): Promise<BeeQueue.Job<void>>;
   complete(job: BeeQueue.Job<any>): Promise<any>;
   process(callback: Function): void;
 }
