5e3831f8dd56011bacc3bc2764b47fe1cc4437b7
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-08-17 12:16:07 +0700

    update

---
+++ src/core/queue/delete-file-queue.example.ts
@@ -0,0 +1,35 @@
+import { Delete, Param } from '@nestjs/common';
+import { BaseQueue } from './base-queue';
+
+export class FileDeleteQueue extends BaseQueue {
+  constructor() {
+    super('file-delete');
+    this.process(async (job) => {
+      const { id } = job.data;
+      //có id rồi sẽ gọi logic delete file ở đây, tốt nhất là viết service riêng rồi gọi
+    });
+  }
+}
+
+//Ví dụ cụ thể ở controller
+
+// @Controller()
+export class FileDeleteControllerExample {
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    const fileDeleteProcessingQueue = new FileDeleteQueue();
+    const job = await fileDeleteProcessingQueue.add(id); //truyền id vào đây để bắt lấy bên trên
+    //nếu ko cần chờ thì return ngay ở đây
+    /* 
+            return {
+                message: 'File cần xoá đã được đưa vào hàng chờ và sẽ dc xoá khi hệ thống sẵn sàng!'
+            }
+        */
+
+    //nếu cần chờ cho job xong mới trả kết quả thì gọi phương thức complete
+    const result = await fileDeleteProcessingQueue.complete(job);
+    return {
+      message: `Đã xoá file có id: ${result.id}`,
+    };
+  }
+}
