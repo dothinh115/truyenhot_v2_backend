beae236dd78b8a125785aea25bc50f8950b8379a
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-10-04 09:21:14 +0700

    update

--- src/core/database/database.module.ts
+++ src/core/database/database.module.ts
@@ -1,15 +1,22 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { connection } from './connection.database';
 
 @Global()
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) =>
-        connection(configService) as any,
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: 5432,
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
       inject: [ConfigService],
     }),
   ],
