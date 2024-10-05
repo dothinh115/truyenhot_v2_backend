afd747de58c82291c4202103a975284a7e9190c8
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-10-05 21:48:53 +0700

    update

--- src/core/asset/asset.service.ts
+++ src/core/asset/asset.service.ts
@@ -68,6 +68,15 @@ export class AssetService {
 
       // So sánh ETag và Last-Modified, nếu khớp, trả về 304 Not Modified
       if (ifNoneMatch === etag || ifModifiedSince === lastModified) {
+        res.header('etag', result.headers['etag']);
+        res.header('last-modified', result.headers['last-modified']);
+        // Đảm bảo cache-control được thiết lập
+        if (query.cache) {
+          const maxAge = parseInt(query.cache, 10);
+          if (!isNaN(maxAge) && maxAge > 0) {
+            res.header('cache-control', `public, max-age=${maxAge}`);
+          }
+        }
         return res.status(304).send(); // Trả về 304 nếu tài nguyên không thay đổi
       }
 
