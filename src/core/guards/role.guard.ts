d6b9b356260261990a1a1dd6a137bd9bce0ae69b
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2025-04-23 11:49:44 +0700

    update

--- src/core/guards/role.guard.ts
+++ src/core/guards/role.guard.ts
@@ -11,20 +11,30 @@ import { Repository } from 'typeorm';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import { ExtendedIncomingMessage } from '../utils/model.util';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
   constructor(
     @InjectRepository(Route) private routeRepo: Repository<Route>,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req: ExtendedIncomingMessage = context.switchToHttp().getRequest();
     //check xem route đang được truy cập có được phân quyền hay ko
     //đối với fastify, req.raw tham chiếu trực tiếp đến InComingMessage, nên gán cho req chính là gán cho raw
-    const { url, method, user } = req;
-    const rawUrl = req.url.split('?')[0].replace(/\/$/, ''); // loại bỏ query và slash cuối
+    const { method, user } = req;
+    const handler = context.getHandler();
+    const controller = context.getClass();
+    const controllerPath =
+      this.reflector.get<string[]>('path', controller) ?? [];
+    const handlerPath = this.reflector.get<string[]>('path', handler) ?? [];
+    const routePattern = '/' + [controllerPath, handlerPath].join('/');
+
+    const rawUrl = routePattern.split('?')[0].replace(/\/$/, '');
+    console.log(rawUrl);
     const routeCacheKey = `route:${rawUrl}:${method}`;
     let currentRoute: Route = await this.cacheManager.get<Route>(routeCacheKey);
     if (!currentRoute) {
@@ -34,11 +44,7 @@ export class RoleGuard implements CanActivate {
           method: method as MethodType,
         },
       });
-      await this.cacheManager.set(
-        routeCacheKey,
-        currentRoute || '__NOT_FOUND__',
-        60000,
-      );
+      await this.cacheManager.set(routeCacheKey, currentRoute || '', 60000);
     }
 
     if (currentRoute && currentRoute.isProtected) {
